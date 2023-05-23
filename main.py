f3d6c89437c82ae2ea9210d6db308f820b3908fa
1nAp <insomniacaptitude@gmail.com>
2023-05-23 22:13:49 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from menu import MENU, resources
+
+print(MENU)
+print(resources)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# TODO: 1. Print report of all coffee machine resources
+
+# TODO: 2. Check if resources are sufficient?
+
+# TODO: 3. Process coins.
+
+# TODO: 4. Check if transaction is successful?
+
+# TODO: 5. Make coffee.
