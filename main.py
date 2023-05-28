a74364ce6f034dd966f76ac528a10ae538650609
1nAp <insomniacaptitude@gmail.com>
2023-05-28 19:01:36 +0200

    Created first part of operate_coffee_machine() function, and support functions check_resources() and print_supplies()

--- main.py
+++ main.py
@@ -1,9 +1,49 @@
+#Imports resources and menu from the menu.py file
 from menu import MENU, resources
-
-print(MENU)
-print(resources)
+import sys
 
 
+def print_supplies(resources, cash):
+    """Prints the remaining resources to the console"""
+    water_left = resources['water']
+    milk_left = resources['milk']
+    coffee_left = resources['coffee']
+    print(f"Water: {water_left}ml\nMilk: {milk_left}ml\nCoffee: {coffee_left}g\nMoney: ${cash}")
+
+def check_resources(resources_left, resources_required):
+    """Subtracts resources from the total. If resources are insufficient, ends program."""
+    for element in resources_left:
+        if element in resources_required:
+            if resources_left[element] >= resources_required[element]:
+                resources_left[element] -= resources_required[element]
+            else:
+                print(f"Sorry, there is not enough {element}.")
+                sys.exit()
+    return resources_left
+
+
+
+
+
+
+
+
+
+
+def operate_coffee_machine():
+    money = 0
+    price_of_beverage = 0
+    global resources
+    run_machine = True
+    while run_machine:
+        coffee_machine_action = input('What would you like? (espresso/latte/cappuccino): ')
+        if coffee_machine_action == 'off':
+            return
+        elif coffee_machine_action == 'report':
+            print_supplies(resources, money)
+        else:
+            resources = check_resources(resources, MENU[coffee_machine_action]['ingredients'])
+            price_of_beverage = MENU[coffee_machine_action]['cost']
 
 
 
@@ -16,6 +56,9 @@ print(resources)
 
 
 
+
+
+operate_coffee_machine()
 
 
 
