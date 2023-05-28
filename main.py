488c2ed7fbb10f601bb2802f5718f3be66a994cf
1nAp <insomniacaptitude@gmail.com>
2023-05-28 20:10:46 +0200

    Created first part of operate_coffee_machine() function, and support functions check_resources() and print_supplies()

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #Imports resources and menu from the menu.py file
 from menu import MENU, resources
-import sys
 
 
 def print_supplies(resources, cash):
@@ -18,21 +17,22 @@ def check_resources(resources_left, resources_required):
                 resources_left[element] -= resources_required[element]
             else:
                 print(f"Sorry, there is not enough {element}.")
-                sys.exit()
+                return False
     return resources_left
 
+def process_coins():
+    """Prompts user to insert coins and calculates the sum."""
+    money = 0
+    coins = {'quarters': 0.25, 'dimes': 0.10, 'nickles': 0.05, 'pennies': 0.01}
+    print("Please insert coins.")
 
-
-
-
-
-
-
-
+    for coin in coins:
+        money += (int(input(f"How many {coin}?: "))) * (coins[coin])
+    return money
 
 def operate_coffee_machine():
-    money = 0
-    price_of_beverage = 0
+    """Operates the coffee_machine and keeps track of profit and resources."""
+    total_money = 0
     global resources
     run_machine = True
     while run_machine:
@@ -40,22 +40,23 @@ def operate_coffee_machine():
         if coffee_machine_action == 'off':
             return
         elif coffee_machine_action == 'report':
-            print_supplies(resources, money)
+            print_supplies(resources, total_money)
         else:
             resources = check_resources(resources, MENU[coffee_machine_action]['ingredients'])
+            if resources == False:
+                return
             price_of_beverage = MENU[coffee_machine_action]['cost']
-
-
-
-
-
-
-
-
-
-
-
-
+            money = process_coins()
+            if money < price_of_beverage:
+                print("Sorry, that's not enough money. Money refunded.")
+                for key in MENU[coffee_machine_action]['ingredients']:
+                    resources[key] += MENU[coffee_machine_action]['ingredients'][key]
+            else:
+                total_money += price_of_beverage
+                change = money - price_of_beverage
+                if change > 0:
+                    print(f'Here is ${change} in change.')
+                print(f'Here is your {coffee_machine_action}. Enjoy!')
 
 
 operate_coffee_machine()
@@ -70,12 +71,12 @@ operate_coffee_machine()
 
 
 
-# TODO: 1. Print report of all coffee machine resources
+# TODO: 1. Print report of all coffee machine resources DONE
 
-# TODO: 2. Check if resources are sufficient?
+# TODO: 2. Check if resources are sufficient? DONE
 
-# TODO: 3. Process coins.
+# TODO: 3. Process coins. DONE
 
-# TODO: 4. Check if transaction is successful?
+# TODO: 4. Check if transaction is successful? DONE
 
-# TODO: 5. Make coffee.
+# TODO: 5. Make coffee. DONE
